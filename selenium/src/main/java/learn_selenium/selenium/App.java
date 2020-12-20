c2424269d666cf1fd5f66e5418ea1c88ec9b98a0
sabbirUlAlam <sabbir.ul.alam@tigerit.com>
2020-12-20 14:53:19 +0600

    how to take a screenshot of a webpage

--- selenium/src/main/java/learn_selenium/selenium/App.java
+++ selenium/src/main/java/learn_selenium/selenium/App.java
@@ -1,4 +1,13 @@
 package learn_selenium.selenium;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
 
 /**
  * Hello world!
@@ -6,8 +15,11 @@ package learn_selenium.selenium;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.example.com");
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("./image.png"));
+        driver.quit();
     }
 }
